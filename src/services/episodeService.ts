66b25422a279797be8d0d9a31caf87dd831e293e
MaxScarlet <maxim.kulik99@gmail.com>
2024-01-30 19:21:12 +0200

    Changes to create in controller

--- src/services/episodeService.ts
+++ src/services/episodeService.ts
@@ -8,39 +8,40 @@ import { SearchParams } from "../models/SearchParams";
 // import { DynamoDbHelper } from '../helpers/dynamoDbHelper';
 
 export class EpisodeService implements CrudApiService<Episode> {
-  // private readonly dbHelper: MongoDbHelper<DistributorDoc> | DynamoDbHelper<Distributor>;
+	// private readonly dbHelper: MongoDbHelper<DistributorDoc> | DynamoDbHelper<Distributor>;
 
-  constructor(private dbHelper: IDbHelper<EpisodeDoc> | IDbHelper<Episode>) {
-    // this.dbHelper = new MongoDbHelper<DistributorDoc>('Distributor', DistributorSchema, tableName);
-    // this.dbHelper = new DynamoDbHelper<Distributor>(tableName);
-  }
+	constructor(private dbHelper: IDbHelper<EpisodeDoc> | IDbHelper<Episode>) {
+		// this.dbHelper = new MongoDbHelper<DistributorDoc>('Distributor', DistributorSchema, tableName);
+		// this.dbHelper = new DynamoDbHelper<Distributor>(tableName);
+	}
 
-  async get_all(queryString: SearchParams): Promise<Episode[] | null> {
-    // check mandatory field - PodcastID
-    if (queryString && !queryString.PodcastID) {
-      return null;
-    }
-    const fields = ["Title", "Description"];
-    const items = await this.dbHelper.get_list<Episode>(queryString, fields);
-    return items;
-  }
+	async get_all(queryString: SearchParams): Promise<Episode[] | null> {
+		// check mandatory field - PodcastID
+		if (queryString && !queryString.PodcastID) {
+			return null;
+		}
+		const fields = ["Title", "Description"];
+		const items = await this.dbHelper.get_list<Episode>(queryString, fields);
+		return items;
+	}
 
-  async get(id: string): Promise<Episode | null> {
-    const item = await this.dbHelper.get<Episode>(id);
-    return item || null;
-  }
+	async get(id: string): Promise<Episode | null> {
+		const item = await this.dbHelper.get<Episode>(id);
+		return item || null;
+	}
 
-  async create(item: Episode): Promise<void> {
-    item.Created = new Date().toISOString();
-    await this.dbHelper.create<Episode>(item);
-  }
+	async create(item: Episode): Promise<Episode> {
+		item.Created = new Date().toISOString();
+		const response = await this.dbHelper.create<Episode>(item);
+		return response;
+	}
 
-  async update(id: string, updated: Episode): Promise<Episode | null> {
-    const item = await this.dbHelper.update<Episode>(id, updated);
-    return item || null;
-  }
+	async update(id: string, updated: Episode): Promise<Episode | null> {
+		const item = await this.dbHelper.update<Episode>(id, updated);
+		return item || null;
+	}
 
-  async delete(id: string): Promise<void> {
-    await this.dbHelper.delete<Episode>(id);
-  }
+	async delete(id: string): Promise<void> {
+		await this.dbHelper.delete<Episode>(id);
+	}
 }
